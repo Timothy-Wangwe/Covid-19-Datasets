e65101ba67b8db6b3ac0703215a3c91eccd9510a
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-03-31 13:22:36 +0300

    Changed auto-comment to 6hrs

--- AutoGit/auto.py
+++ AutoGit/auto.py
@@ -3,6 +3,6 @@ def git():
 
     system("git pull")
     system("git add *")
-    system("git commit -m 'Automated hourly dataset update'")
+    system("git commit -m 'Automated every 6th hour dataset update'")
     system("git push")
     system("git status")
