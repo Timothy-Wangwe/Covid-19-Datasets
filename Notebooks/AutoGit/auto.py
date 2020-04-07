8845456b583d11c4af129ac539b723eaf381fcf6
T-Skipper <wangwetymothy@gmail.com>
2020-04-08 00:10:04 +0300

    Automated every 6th hour dataset update

--- Notebooks/AutoGit/auto.py
+++ Notebooks/AutoGit/auto.py
@@ -2,7 +2,7 @@ def git():
     from os import system
 
     system("git pull")
-    system("git add *")
+    system("git add Data-Sets/*")
     system("git commit -m 'Automated every 6th hour dataset update'")
     system("git push")
     system("git status")
